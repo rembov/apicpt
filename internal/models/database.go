629b4508091ddfa21527559961802a2da84bb4cf
rembov <i7888@mail.ru>
2025-01-31 10:32:51 +0300

    eb mozgi

--- internal/models/database.go
+++ internal/models/database.go
@@ -1,21 +1,21 @@
 package models
 
 type DBConfig struct {
-    Host     string
-    Port     string
-    User     string
-    Password string
-    DBName   string
-    SSLMode  string
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
 }
 
 func GetDBConfig() DBConfig {
-    return DBConfig{
-        Host:     "127.0.0.1",
-        Port:     "5432",
-        User:     "postgres",
-        Password: "AuX2313gdr",
-        DBName:   "Negri",
-        SSLMode:  "disable",
-    }
+	return DBConfig{
+		Host:     "127.0.0.1",
+		Port:     "5432",
+		User:     "postgres",
+		Password: "negr321",
+		DBName:   "Negri",
+		SSLMode:  "disable",
+	}
 }
